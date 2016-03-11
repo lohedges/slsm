2c2a0f3fe60a713d6e6a37a5a0826a95e8b5272b
Lester Hedges <lester.hedges@gmail.com>
2016-03-11 12:25:48 +0000

    Added license.

--- src/Sensitivity.h
+++ src/Sensitivity.h
@@ -1,7 +1,19 @@
 /*
- * File:	Sensitivity.h
- * Author:	lester
- */
+  Copyright (c) 2015-2016 Lester Hedges <lester.hedges+lsm@gmail.com>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef _SENSITIVITY_H
 #define _SENSITIVITY_H
@@ -32,11 +44,11 @@ namespace lsm
     public:
         //! Constructor.
         /*! \param delta_
-                The finite difference derivative length.
+                The finite-difference derivative length.
         */
         Sensitivity(double delta_ = 0.1);
 
-        //! Compute finite difference sensitivity for an arbitrary function.
+        //! Compute finite-difference sensitivity for an arbitrary function.
         /*! \param point
                 A reference to the boundary point.
 
