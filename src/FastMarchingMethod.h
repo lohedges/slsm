62f898a038a73f5993e7d60fe9b7a9f560df3059
Lester Hedges <lester.hedges@gmail.com>
2016-11-18 11:37:42 +0000

    Added scikit-fmm to copyright list.

--- src/FastMarchingMethod.h
+++ src/FastMarchingMethod.h
@@ -1,5 +1,6 @@
 /*
   Copyright (c) 2015-2016 Lester Hedges <lester.hedges+lsm@gmail.com>
+  Copyright (c) 2012 the scikit-fmm team
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
