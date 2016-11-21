f7406327f241dc8231069b9df9af24701d81e892
Lester Hedges <lester.hedges@gmail.com>
2016-11-21 14:59:23 +0000

    Updated email address.

--- tests/fast_marching_tests.cpp
+++ tests/fast_marching_tests.cpp
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2015-2016 Lester Hedges <lester.hedges+lsm@gmail.com>
+  Copyright (c) 2015-2016 Lester Hedges <lester.hedges+slsm@gmail.com>
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
