3fabe4f49aa4c5c19295f246947de6872c11f005
Lester Hedges <lester.hedges@gmail.com>
2018-08-01 11:20:55 +0100

    Fix call to slsm_log_err. [ci skip]

--- src/MinUnit.h
+++ src/MinUnit.h
@@ -13,7 +13,7 @@
 
 #define mu_suite_start() int message = 0
 
-#define mu_assert(test, message) if (!(test)) { log_err(message); return message; }
+#define mu_assert(test, message) if (!(test)) { slsm_log_err(message); return message; }
 #define mu_run_test(test) slsm_debug("\n-----%s", " " #test); \
     message = test(); tests_run++; if (message) return message;
 
